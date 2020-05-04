636b47aa157af01165b867b0da14f0bb79e70d76
MrBlackTie <brianbassard@gmail.com>
2020-05-04 10:35:12 -0700

    Lesson Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,55 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Magic 8 Ball'),
+          backgroundColor: Colors.grey[800],
+        ),
+        body: MagicPage(),
       ),
+    ),
+  );
+}
+
+class MagicPage extends StatefulWidget {
+  @override
+  _MagicPageState createState() => _MagicPageState();
+}
+
+class _MagicPageState extends State<MagicPage> {
+  int ballAnswer = 1;
+
+  void shakeBall() {
+    setState(() {
+      ballAnswer = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              onPressed: () {
+                shakeBall();
+              },
+              child: Image.asset('images/ball$ballAnswer.png'),
+            )
+          ],
+        ),
+      ),
+      duration: Duration(seconds: 2),
     );
+  }
+}
